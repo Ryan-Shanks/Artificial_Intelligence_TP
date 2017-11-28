2964a56e49a9461e33ebcf8e055a49ac1db44b65
Ryan-Shanks <ryan.w.shanks@gmail.com>
2017-11-27 21:08:46 -0500

    Added a comment

--- SGA/src/SGA.java
+++ SGA/src/SGA.java
@@ -3,19 +3,23 @@ public class SGA {
 	private Problem prob;
 	Individual[] pop;
 	private int popSize;
+
 	public SGA(Problem p, int popSize) {
 		prob = p;
 		this.popSize = popSize;
 		pop = new Individual[popSize];
-		
+
 	}
+
 	private void testPopFitness() {
 		for (Individual p : pop) {
 			p.calcFitness();
 		}
 	}
+
 	private void biasedRoulette() {
 		testPopFitness();
-		//TODO complete the biased roulette.s
+		// TODO complete the biased roulette, and either return the results or call a
+		// function from here to generate the population
 	}
 }
