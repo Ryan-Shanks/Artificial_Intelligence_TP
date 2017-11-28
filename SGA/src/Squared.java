c04eb4629fa25aa8ea3b88fc2c56e147d932c451
Ryan-Shanks <ryan.w.shanks@gmail.com>
2017-11-27 21:07:01 -0500

    Added a test problem to find the largest 16 bit number squared

---
+++ SGA/src/Squared.java
@@ -0,0 +1,27 @@
+/**
+ * basic test class. Problem is to find the largest 16 bit number squared. 0th
+ * bit is the MSB
+ * 
+ * @author ryanw
+ *
+ */
+public class Squared implements Problem {
+	public double fitness(boolean[] candidate) {
+		double result = 0;
+		for (int p = 15; p >= 0; p--) {
+			if (candidate[15 - p]) {
+				result += Math.pow(2, p);
+			}
+		}
+		return Math.pow(result, 2);
+	}
+
+	public int getNumChromosomes() {
+		return 16;
+	}
+
+	public int getMaxOrMin() {
+		return 1;
+	}
+
+}
