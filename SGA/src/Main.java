c5bcb08da411e8ae3e7f3b25be0f85ecfab2688d
Ryan-Shanks <ryan.w.shanks@gmail.com>
2017-11-27 20:50:53 -0500

    Added dynamic class loading in main

--- SGA/src/Main.java
+++ SGA/src/Main.java
@@ -1,6 +1,7 @@
 
 public class Main {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+		Problem p = (Problem) Class.forName(args[0]).newInstance();
+		SGA s = new SGA(p, 100);
 	}
 }
