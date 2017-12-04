37f34d784d156b891c8fcc7ce4292cef045c684e
Ryan-Shanks <ryan.w.shanks@gmail.com>
2017-12-04 16:09:09 -0500

    Minor change to the index in dejong, shouldnt affect output

--- SGA/src/DeJongSphere.java
+++ SGA/src/DeJongSphere.java
@@ -6,7 +6,7 @@ public class DeJongSphere implements Problem {
 	public double fitness(boolean[] candidate) {
 		float fitness = 0, floatValue;
 		boolean[] portion = new boolean[32];
-		for (int i = 0; i < candidate.length / 32; i++) {
+		for (int i = 0; i < n; i++) {
 			System.arraycopy(candidate, i * 32, portion, 0, 32);
 			floatValue = OFHelper.floatFromBoolArray(portion);
 			fitness += Math.pow(floatValue, 2);
