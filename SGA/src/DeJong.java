43daaeba2fe9468ede2116188baa1e3d8fcd0c56
Talha-Mahmud <32148649+Talha-Mahmud@users.noreply.github.com>
2017-12-04 01:39:12 -0500

    Add files via upload

--- SGA/src/DeJong.java
+++ SGA/src/DeJong.java
@@ -6,7 +6,7 @@ public class DeJong implements Problem{
 		float fitness = 0, floatValue;
 		boolean[] portion = new boolean[32];
 		for (int i = 0; i < candidate.length/32; i++) {
-			System.arraycopy(candidate, i, portion, 0, 32);
+			System.arraycopy(candidate, i *32, portion, 0, 32);
 			floatValue = OFHelper.floatFromBoolArray(portion);
 			fitness += Math.pow(floatValue, 2);
 		}
