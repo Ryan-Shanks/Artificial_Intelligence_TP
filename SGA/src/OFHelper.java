111d55b25bdfb5dcacf4eca32c9c5f137041b596
Ryan-Shanks <ryan.w.shanks@gmail.com>
2017-12-04 00:14:57 -0500

    Got bool to float working

--- SGA/src/OFHelper.java
+++ SGA/src/OFHelper.java
@@ -20,4 +20,23 @@ public class OFHelper {
 		}
 		return out;
 	}
+	/**
+	 * takes a boolean vector of length 32 converts it to a float
+	 * @return
+	 */
+	public static float floatFromBoolArray(boolean[] input) {
+		if (input.length != 32) {
+			throw new IllegalArgumentException("FloatFromBoolArray recieved an array that was not 32 bits long!");
+		}
+		int val = 0;
+		for (int i =0; i < 32; i++) {
+			if(input[i]) {
+				val++;
+			}
+			val = val << 1;
+		}
+		//so now val is 4 bytes and contains the 32 boolean bits.
+		// now we need to somehow represent it as a float without casting it.
+		return Float.intBitsToFloat(val);
+	}
 }
