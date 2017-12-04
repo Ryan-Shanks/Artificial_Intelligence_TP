63146168359fe0fc4377982c419e7d8b3f7210c0
Talha-Mahmud <32148649+Talha-Mahmud@users.noreply.github.com>
2017-12-04 01:37:27 -0500

    Add files via upload

---
+++ DeJong.java
@@ -0,0 +1,25 @@
+
+public class DeJong implements Problem{
+	int n = 2;
+	@Override
+	public double fitness(boolean[] candidate) {
+		float fitness = 0, floatValue;
+		boolean[] portion = new boolean[32];
+		for (int i = 0; i < candidate.length/32; i++) {
+			System.arraycopy(candidate, i, portion, 0, 32);
+			floatValue = OFHelper.floatFromBoolArray(portion);
+			fitness += Math.pow(floatValue, 2);
+		}
+		return fitness;
+	}
+
+	@Override
+	public int getNumChromosomes() {
+		return n * 32;
+	}
+
+	@Override
+	public int getMaxOrMin() {
+		return -1;
+	}
+}
