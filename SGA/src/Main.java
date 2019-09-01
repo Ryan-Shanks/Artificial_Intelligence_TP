491e81c9e8fc8321ff623b5043576ea5750ab765
Ryan Shanks <ryan.w.shanks@gmail.com>
2019-08-31 20:32:15 -0400

    Replace deprecated method

--- SGA/src/Main.java
+++ SGA/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 	private final static int NUM_GENERATIONS = 1000000; // if we have not found a better candidate in this many generations, done
 	public static void main(String[] args) throws Exception {
-		Problem p = (Problem) Class.forName(args[0]).newInstance();
+		Problem p = (Problem) Class.forName(args[0]).getConstructor().newInstance();
 		SGA s = new SGA(p, 100);
 		s.run(NUM_GENERATIONS, false);
 	}
